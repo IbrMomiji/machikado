9a488de4c79f7f87a60cdbcb6fe73e14bd4c639c
Ibara Babanyanru Rihyosil <140140577+IbrMomiji@users.noreply.github.com>
2024-11-24 07:54:29 +0900

    v15

--- themes/script/after.js
+++ themes/script/after.js
@@ -8,82 +8,94 @@ $('main').scrollgress({
 /* ================================================ */
 /* ヘッダー部分 */
 /* ================================================ */
-$(function() {
+$(function () {
     var elemTop = [];
     function PositionCheck() {
         var headerH = $("#header").outerHeight(true);
-        $(".scroll-point").each(function(i) {
+        $(".scroll-point").each(function (i) {
             elemTop[i] = Math.round(parseInt($(this).offset().top - headerH));
         });
     }
     function ScrollAnime() {
         var scroll = Math.round($(window).scrollTop());
         var NavElem = $("#g-nav li");
-        $("#g-nav li").removeClass('current'); 
+        $("#g-nav li").removeClass('current');
         if (scroll >= 0 && scroll < elemTop[1]) {
-            $(NavElem[0]).addClass('current'); 
+            $(NavElem[0]).addClass('current');
         } else if (scroll >= elemTop[1] && scroll < elemTop[2]) {
-            $(NavElem[1]).addClass('current'); 
+            $(NavElem[1]).addClass('current');
         } else if (scroll >= elemTop[2] && scroll < elemTop[3]) {
             $(NavElem[2]).addClass('current');
         } else if (scroll >= elemTop[3]) {
-            $(NavElem[3]).addClass('current'); 
+            $(NavElem[3]).addClass('current');
         }
     }
-    $('#g-nav a').click(function() {
-        var elmHash = $(this).attr('href'); 
-        var headerH = $("#header").outerHeight(true); 
+    $('#g-nav a').click(function () {
+        var elmHash = $(this).attr('href');
+        var headerH = $("#header").outerHeight(true);
         var pos = Math.round($(elmHash).offset().top - headerH);
-        $('body,html').animate({scrollTop: pos}, 500); 
+        $('body,html').animate({ scrollTop: pos }, 500);
         return false;
     });
-    $(window).scroll(function() { PositionCheck(); ScrollAnime(); });
-    $(window).on('load', function() { PositionCheck(); ScrollAnime(); });
-    $(window).resize(function() { PositionCheck() }); 
+    $(window).scroll(function () { PositionCheck(); ScrollAnime(); });
+    $(window).on('load', function () { PositionCheck(); ScrollAnime(); });
+    $(window).resize(function () { PositionCheck() });
 });
+/* temp */
+
+$(".openbtn").click(function () {//ボタンがクリックされたら
+    $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#hamburgermenu").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#hamburgermenu a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#hamburgermenu").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
+
 /* ================================================ */
 /* スクロール画面移動 */
 /* ================================================ */
 $.scrollify({
-    section : ".scroll-point",//1ページスクロールさせたいエリアクラス名
-    interstitialSection : "#header,#footer",//ヘッダーフッターを認識し、1ページスクロールさせず表示されるように設定
+    section: ".scroll-point",//1ページスクロールさせたいエリアクラス名
+    interstitialSection: "#header,#footer",//ヘッダーフッターを認識し、1ページスクロールさせず表示されるように設定
     easing: "swing", // 他にもlinearやeaseOutExpoといったjQueryのeasing指定可能
     scrollSpeed: 300, // スクロール時の速度
-    
+
     //以下、ページネーション設定
-    before:function(i,panels) {
-    var ref = panels[i].attr("data-url");
-    $(".pagination .active").removeClass("active");
-    $(".pagination").find("a[href=\"?" + ref + "\"]").addClass("active");
+    before: function (i, panels) {
+        var ref = panels[i].attr("data-url");
+        $(".pagination .active").removeClass("active");
+        $(".pagination").find("a[href=\"?" + ref + "\"]").addClass("active");
     },
-    afterRender:function() {
-    var pagination = "<ul class=\"pagination\">";
-    var activeClass = "";
-    $(".scroll-point").each(function(i) {//1ページスクロールさせたいエリアクラス名を指定
-    activeClass = "";
-    if(i===$.scrollify.currentIndex()) {
-    activeClass = "active";
+    afterRender: function () {
+        var pagination = "<ul class=\"pagination\">";
+        var activeClass = "";
+        $(".scroll-point").each(function (i) {//1ページスクロールさせたいエリアクラス名を指定
+            activeClass = "";
+            if (i === $.scrollify.currentIndex()) {
+                activeClass = "active";
+            }
+            pagination += "<li><a class=\"" + activeClass + "\" href=\"?" + $(this).attr("data-url") + "\"><span class=\"hover-text\">" + $(this).attr("data-url").charAt(0).toUpperCase() + $(this).attr("data-url").slice(1) + "</span></a></li>";
+        });
+        pagination += "</ul>";
+
+        $("#area-1").append(pagination);//はじめのエリアにページネーションを表示
+        $(".pagination a").on("click", $.scrollify.move);
     }
-    pagination += "<li><a class=\"" + activeClass + "\" href=\"?" + $(this).attr("data-url") + "\"><span class=\"hover-text\">" + $(this).attr("data-url").charAt(0).toUpperCase() + $(this).attr("data-url").slice(1) + "</span></a></li>";
-    });
-    pagination += "</ul>";
-    
-    $("#area-1").append(pagination);//はじめのエリアにページネーションを表示
-    $(".pagination a").on("click",$.scrollify.move);
-    }
-    
-    });
+
+});
 /* ================================================ */
 /* トップへ戻る */
 /* ================================================ */
 function PageTopAnime() {
     var scroll = $(window).scrollTop();
-    if (scroll >= 200){
+    if (scroll >= 200) {
         $('#page-top').removeClass('RightMove');
         $('#page-top').addClass('LeftMove');
-    }else{
-        if(
-            $('#page-top').hasClass('LeftMove')){
+    } else {
+        if (
+            $('#page-top').hasClass('LeftMove')) {
             $('#page-top').removeClass('LeftMove');
             $('#page-top').addClass('RightMove');
         }
@@ -95,6 +107,6 @@ $(window).on('load', function () { PageTopAnime(); });
 $('#page-top').click(function () {
     $('body,html').animate({
         scrollTop: 0
-    }, 1500,"easeInOutQuint");
+    }, 1500, "easeInOutQuint");
     return false;
 });
--- themes/script/main.js
+++ themes/script/main.js
@@ -1,19 +1,19 @@
 const ts = performance.now();
 /* ======================== TEST ======================== */
 console.warn("%c見ないで", "font-size:100px; color:red;");
-console.assert(document.getElementById("debug"), "'debug'IDはありません"); 
+console.assert(document.getElementById("debug"), "'debug'IDはありません");
 /* ================= IE user Redirect =================== */
 var userAgent = window.navigator.userAgent.toLowerCase();
-if(userAgent.indexOf('msie') != -1 || userAgent.indexOf('trident') != -1) {
-    window.location.href('pages/IE.html')
+if (userAgent.indexOf('msie') != -1 || userAgent.indexOf('trident') != -1) {
+  window.location.href('pages/IE.html')
 }
 /* ====================== HTML get ====================== */
 const elements = document.querySelectorAll('*');
 const elementData = Array.from(elements).map((element, index) => {
   return {
     index: index + 1,
-    name: element.tagName.toLowerCase(), 
-    line: getLineNumberOfElement(element), 
+    name: element.tagName.toLowerCase(),
+    line: getLineNumberOfElement(element),
   };
 });
 console.table(elementData);
@@ -31,31 +31,31 @@ const te = performance.now();
 console.log('disposal time :: ', (te - ts).toFixed(2), 'ms');
 /* ===================== COOKIE TEST ===================== */
 function trackPageNavigation() {
-    let pageVisits = parseInt(getCookie('pageVisits')) || 0;
-    pageVisits++;
-    setCookie('pageVisits', pageVisits, 30);
-    console.log('ページ移動回数:', pageVisits);
-  }
-  window.onload = trackPageNavigation;
-  window.addEventListener('popstate', trackPageNavigation);
-  window.addEventListener('pushstate', trackPageNavigation);
-  window.addEventListener('replacestate', trackPageNavigation);
-  function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-  }
-  function setCookie(name, value, days) {
-    const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    const expires = `; expires=${date.toUTCString()}`;
-    document.cookie = `${name}=${value}${expires}; path=/`;
-  }
+  let pageVisits = parseInt(getCookie('pageVisits')) || 0;
+  pageVisits++;
+  setCookie('pageVisits', pageVisits, 30);
+  console.log('ページ移動回数:', pageVisits);
+}
+window.onload = trackPageNavigation;
+window.addEventListener('popstate', trackPageNavigation);
+window.addEventListener('pushstate', trackPageNavigation);
+window.addEventListener('replacestate', trackPageNavigation);
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
+function setCookie(name, value, days) {
+  const date = new Date();
+  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+  const expires = `; expires=${date.toUTCString()}`;
+  document.cookie = `${name}=${value}${expires}; path=/`;
+}
 /* ======================== YOUR IP ======================= */
 $.ajax({
-    url: "https://ipinfo.io",
-    dataType: "jsonp",
-    success: function(res){
-         $(".ip-address").text(res.ip);
-    }
+  url: "https://ipinfo.io",
+  dataType: "jsonp",
+  success: function (res) {
+    $(".ip-address").text(res.ip);
+  }
 });
